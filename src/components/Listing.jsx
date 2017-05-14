ec87a95b91e90e6b8ee15451ffce5e70a8eb61ca
hammad_shakil <hammadshakil109@gmail.com>
2017-05-14 10:44:17 +0500

    Driver card updated

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -6,11 +6,17 @@ class Listing extends Component {
     return (
       <div className="row card">
         <div className="col-md-9 info">
-          <a href=""><h4>{this.props.driver.id}</h4></a>
-          <p>what are you doing bro.</p>
+          <h4>Name : {this.props.driver.name}</h4>
+          <h4>Phone No : {this.props.driver.phone}</h4>
+          <h4>No of Customers : {this.props.driver.subscriptions}</h4>
+          <h4>Time 1 : {this.props.driver.pickupTime1}</h4>
+          <h4>Time 2 : {this.props.driver.pickupTime2}</h4>
+          <h4>Capacity : {this.props.driver.capacity}</h4>
+          <h4>Car Name : {this.props.driver.carName}</h4>
+          <h4>Time 2 : {this.props.driver.pickupTime2}</h4>
       </div>
       <div className="col-md-3 contact">
-        <p><a href="callTo:03452237077" className="call-to">03452237077</a></p>
+        <p className="btn btn-success" style={{marginTop: '20px'}}>Request</p>
       </div>
       </div>
     );
