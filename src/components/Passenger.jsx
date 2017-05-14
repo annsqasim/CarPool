dbd26461313fe544c7bbd11342695cf65a6ebad8
Qasim <muhammed.bin-qasim@vistajet.com>
2017-05-14 05:32:45 +0500

    changed passenger page for search

--- src/components/Passenger.jsx
+++ src/components/Passenger.jsx
@@ -7,7 +7,25 @@ class Passenger extends Component {
     return (
       <div className="App">
       <Header />
-        IN app
+        <div className="container get-the-app-content zoom-out">
+            <div className="row">
+                <div className="col-sm-12 text-center">
+                    <h1 className="sign-in-page-heading">Find Drivers Near Your Route.</h1>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+                    <div className="fields-section text-center">
+                        <div className="btn-group rsvp-options dropdown">
+                            <input
+                                className='locationSearch'
+			                    type='text'
+			                    placeholder='Location'
+			                    onChange={event => this.setState({location: event.target.value})}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
